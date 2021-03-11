046a79fc3ec09823010c1928d58b2fb5da58f07f
Christine Coan <79032413+ChristineCoan@users.noreply.github.com>
2021-03-10 19:26:39 -0500

    Add files via upload

---
+++ hw01/Problem1.java
@@ -0,0 +1,9 @@
+public class Problem1{
+    public static void main(String[] args) {
+        double a=3;
+        double b=4;
+        double c=5;
+        double Dis = ((Math.pow(b,2))-4*a*c);
+        System.out.println("The discriminant is "+ Dis);
+    }
+}

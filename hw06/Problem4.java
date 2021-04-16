14b912c83e9fb531bb771241349b96e8b8f24eed
Christine Coan <79032413+ChristineCoan@users.noreply.github.com>
2021-04-16 14:42:55 -0400

    Add files via upload

--- hw06/Problem4.java
+++ hw06/Problem4.java
@@ -3,7 +3,7 @@ public class Problem4 {
     public static void main(String[] args) {
         int i; 
         int j;
-		float det, temp;
+		float det;
 		float mat[][] = new float[2][2];
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a, b, c, d: ");
@@ -11,14 +11,25 @@ public class Problem4 {
 			for(j = 0; j < 2; ++j)
 				mat[i][j] = in.nextFloat();
 		det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
+		for(i = 0; i < 2; ++i)
+			for(j = 0; j < 2; ++j)
+				System.out.print((mat[i][j]/det) +" ");
+	}
+		
+	public static double[][] inverse(double[][] A){
+		int i; 
+        int j;
+		float det, temp;
+		float mat[][] = new float[2][2];
+		det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
 		temp = mat[0][0];
 		mat[0][0] = mat[1][1];
 		mat[1][1] = temp;
 		mat[0][1] = - mat[0][1];
 		mat[1][0] = - mat[1][0];
-		for(i = 0; i < 2; ++i) {
+		for(i = 0; i < 2; ++i)
 			for(j = 0; j < 2; ++j)
 				System.out.print((mat[i][j]/det) +" ");
-		}
+		return inverse(A);
 	}
 }

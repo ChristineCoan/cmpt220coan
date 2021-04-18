82f991d7431864fece59a074a1078f61c940c016
Juan Arias <juan.arias@marist.edu>
2021-04-18 17:02:13 -0400

    Feedback

--- hw06/Problem4.java
+++ hw06/Problem4.java
@@ -1,35 +1,38 @@
 import java.util.*;
+
 public class Problem4 {
-    public static void main(String[] args) {
-        int i; 
-        int j;
-		float det;
-		float mat[][] = new float[2][2];
+	public static void main(String[] args) {
+		int i;
+		int j;
+		// float det;
+		double mat[][] = new double[2][2];
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a, b, c, d: ");
-		for(i = 0; i < 2; ++i)
-			for(j = 0; j < 2; ++j)
-				mat[i][j] = in.nextFloat();
-		det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
-		for(i = 0; i < 2; ++i)
-			for(j = 0; j < 2; ++j)
-				System.out.print((mat[i][j]/det) +" ");
+		for (i = 0; i < 2; ++i)
+			for (j = 0; j < 2; ++j)
+				mat[i][j] = in.nextDouble();
+		// det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
+		double[][] inverse = inverse(mat);
+		for (i = 0; i < 2; ++i) {
+			for (j = 0; j < 2; ++j)
+				System.out.print((inverse[i][j]) + " ");
+			System.out.println();
+		}
 	}
-		
-	public static double[][] inverse(double[][] A){
-		int i; 
-        int j;
-		float det, temp;
-		float mat[][] = new float[2][2];
+
+	public static double[][] inverse(double[][] mat) {
+		int i;
+		int j;
+		double det;
+		double[][] inv = new double[2][2];
 		det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
-		temp = mat[0][0];
-		mat[0][0] = mat[1][1];
-		mat[1][1] = temp;
-		mat[0][1] = - mat[0][1];
-		mat[1][0] = - mat[1][0];
-		for(i = 0; i < 2; ++i)
-			for(j = 0; j < 2; ++j)
-				System.out.print((mat[i][j]/det) +" ");
-		return inverse(A);
+		inv[0][0] = mat[1][1];
+		inv[1][1] = mat[0][0];
+		inv[0][1] = -mat[0][1];
+		inv[1][0] = -mat[1][0];
+		for (i = 0; i < 2; ++i)
+			for (j = 0; j < 2; ++j)
+				inv[i][j] = inv[i][j] / det;
+		return inv;
 	}
 }

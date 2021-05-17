cd74880d9393e472492b4170aa5146a05e5806d9
Juan Arias <juan.arias@marist.edu>
2021-05-17 09:32:57 -0400

    Feedback

--- hw10/Problem2.java
+++ hw10/Problem2.java
@@ -1,55 +1,65 @@
 public class Problem2 {
-    //had an error with vs code and running the programs didnt say an error but couldnt run to check
-    //class Animal which implements Comparable and Cloneable
-public class Animal implements Comparable<Animal>, Cloneable{
+
+    // driver class to test the above example
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Animal[] list = new Animal[5];
+        list[0] = new Chicken(); // creating a new Chicken object
+        list[0].setWeight(4.5); // setting the weight to 4.5
+        list[1] = new Tiger(); // creating a new Tiger object
+        list[1].setWeight(46.6); // setting the weight to 46.6
+        list[2] = new Chicken(); // creating a new Chicken object
+        list[2].setWeight(1.5); // setting the weight to 1.5
+        list[3] = (Animal) (list[0].clone());// creating a clone of the first element and assigning to the 4th
+                                             // element
+        list[3].setWeight(7.5);// setting the weight to 7.5
+        list[4] = (Animal) (list[1].clone()); // creating a clone of the 2nd element and assigning to the 5th
+                                              // element
+
+        java.util.Arrays.sort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println("weight: " + list[i].getWeight());
+        }
+
+    }
+}
+
+// had an error with vs code and running the programs didnt say an error but
+// couldnt run to check
+// class Animal which implements Comparable and Cloneable
+class Animal implements Comparable<Animal>, Cloneable {
     private double weight;// weight variable to store the weight
-    //getter method to get the weight
+    // getter method to get the weight
+
     public double getWeight() {
         return weight;
     }
-    //getter method to set the weight
+
+    // getter method to set the weight
     public void setWeight(double weight) {
         this.weight = weight;
     }
+
     // overriding the compareTo method of the Comparable class
     @Override
     public int compareTo(Animal animal) {
-        double animalWeight= animal.getWeight();// getting the weight of the animal obhect
-        return (int) (this.getWeight()-animalWeight);// sorting in ascending order
+        double animalWeight = animal.getWeight();// getting the weight of the animal obhect
+        return (int) (this.getWeight() - animalWeight);// sorting in ascending order
     }
+
     // overriding the clone method of the Cloneable class
-    //throws CloneNotSupportedException exception
+    // throws CloneNotSupportedException exception
     @Override
     protected Object clone() throws CloneNotSupportedException {
         // TODO Auto-generated method stub
         return super.clone();
-    } 
- }
-    // dummy Chicken class which extends Animal
-    class Chicken extends Animal{
-    }
-    // dummy Tiger class which extends Animal
-    class Tiger extends Animal{
-    }
-    // driver class to test the above example
-    public class AnimalTest {
-        public  void main(String[] args) throws CloneNotSupportedException {
-            Animal [] list= new Animal[5];
-            list[0]= new Chicken(); // creating a new Chicken object
-            list[0].setWeight(4.5); // setting the weight to 4.5
-            list[1]= new Tiger(); // creating a new Tiger object
-            list[1].setWeight(46.6); // setting the weight to 46.6
-            list[2]= new Chicken(); // creating a new Chicken object
-            list[2].setWeight(1.5); // setting the weight to 1.5
-            list[3]= (Animal) (list[0].clone());// creating a clone of the first element and assigning to the 4th element
-            list[3].setWeight(7.5);// setting the weight to 7.5
-            list[4]= (Animal) (list[1].clone()); // creating a clone of the 2nd element and assigning to the 5th element
-        
-            java.util.Arrays.sort(list);
-            for(int i=0;i<list.length;i++){
-                System.out.println("weight: "+list[i].getWeight());
-            }
-        }
     }
 }
- 
+
+// dummy Chicken class which extends Animal
+class Chicken extends Animal {
+}
+
+// dummy Tiger class which extends Animal
+class Tiger extends Animal {
+}
--- hw10/Problem4.java
+++ hw10/Problem4.java
@@ -1,133 +1,151 @@
 import java.util.*;
+
 //Problem3 not sure why i put 4
 public class Problem4 {
-//had an error with vs code and running the programs didnt say an error but couldnt run to check
-    public class RationalTest {
-    public void main(String[] args) {
+        // had an error with vs code and running the programs didnt say an error but
+        // couldnt run to check
+        public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
-                
+
                 System.out.print("Enter the first rational number: ");
                 String rational1 = sc.nextLine();
-                
+
                 System.out.print("Enter the second rational number: ");
                 String rational2 = sc.nextLine();
-                
+
                 sc.close();
-                
+
                 Rational rat1 = Rational.parseRationalNumber(rational1);
                 Rational rat2 = Rational.parseRationalNumber(rational2);
-                
-                System.out.println(rat1+" + "+rat2+" = "+rat1.add(rat2));
-    }
-}
-    public static class Rational extends Number implements Comparable<Rational> {
-        // Data fields for numerator and denominator
-        private long numerator = 0;
-        private long denominator = 1;
-        //Construct a rational with default properties
-        public Rational() {
-                this(0, 1);
-        }
-        //Construct a rational with specified numerator and denominator
-        public Rational(long numerator, long denominator) {
-                long gcd = gcd(numerator, denominator);
-                this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
-                this.denominator = Math.abs(denominator) / gcd;
-        }
-        //Find GCD of two numbers
-        private long gcd(long n, long d) {
-                long n1 = Math.abs(n);
-                long n2 = Math.abs(d);
-                int gcd = 1;
 
-                for (int k = 1; k <= n1 && k <= n2; k++) {
-                        if (n1 % k == 0 && n2 % k == 0)
-                                gcd = k;
+                System.out.println(rat1 + " + " + rat2 + " = " + rat1.add(rat2));
+        }
+
+        public static class Rational extends Number implements Comparable<Rational> {
+                // Data fields for numerator and denominator
+                private long numerator = 0;
+                private long denominator = 1;
+
+                // Construct a rational with default properties
+                public Rational() {
+                        this(0, 1);
                 }
 
-                return gcd;
-        }
-        //Return numerator
-        public long getNumerator() {
-                return numerator;
-        }
-        //Return denominator
-        public long getDenominator() {
-                return denominator;
-        }
-        //Add a rational number to this rational
-        public Rational add(Rational secondRational) {
-                long n = numerator * secondRational.getDenominator() + denominator
-                                * secondRational.getNumerator();
-                long d = denominator * secondRational.getDenominator();
-                return new Rational(n, d);
-        }
-        //Subtract a rational number from this rational
-        public Rational subtract(Rational secondRational) {
-                long n = numerator * secondRational.getDenominator() - denominator
-                                * secondRational.getNumerator();
-                long d = denominator * secondRational.getDenominator();
-                return new Rational(n, d);
-        }
-        //Multiply a rational number to this rational
-        public Rational multiply(Rational secondRational) {
-                long n = numerator * secondRational.getNumerator();
-                long d = denominator * secondRational.getDenominator();
-                return new Rational(n, d);
-        }
-        //Divide a rational number from this rational
-        public Rational divide(Rational secondRational) {
-                long n = numerator * secondRational.getDenominator();
-                long d = denominator * secondRational.numerator;
-                return new Rational(n, d);
-        }
-        public String toString() {
-                if (denominator == 1)
-                        return numerator + "";
-                else
-                        return numerator + "/" + denominator;
-        }
-        // Override the equals method in the Object class
-        public boolean equals(Object other) {
-                if ((this.subtract((Rational) (other))).getNumerator() == 0)
-                        return true;
-                else
-                        return false;
-        }
-        // Implement the abstract intValue method in Number
-        public int intValue() {
-                return (int) doubleValue();
-        }
-        // Implement the abstract floatValue method in Number
-        public float floatValue() {
-                return (float) doubleValue();
-        }
-        // Implement the doubleValue method in Number
-        public double doubleValue() {
-                return numerator * 1.0 / denominator;
-        }
-        // Implement the abstract longValue method in Number
-        public long longValue() {
-                return (long) doubleValue();
-        }
-        // Implement the compareTo method in Comparable
-        public int compareTo(Rational o) {
-                if (this.subtract(o).getNumerator() > 0)
-                        return 1;
-                else if (this.subtract(o).getNumerator() < 0)
-                        return -1;
-                else
-                        return 0;
-        }
-        public static Rational parseRationalNumber(String s) {
-                if (s.contains("/")) {
-                        String words[] = s.split("/");
-                        long numerator = Long.parseLong(words[0].trim());
-                        long denominator = Long.parseLong(words[1].trim());
-                        return new Rational(numerator, denominator);
-                } else {
-                        return new Rational(Long.parseLong(s.trim()), 1);
+                // Construct a rational with specified numerator and denominator
+                public Rational(long numerator, long denominator) {
+                        long gcd = gcd(numerator, denominator);
+                        this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
+                        this.denominator = Math.abs(denominator) / gcd;
+                }
+
+                // Find GCD of two numbers
+                private long gcd(long n, long d) {
+                        long n1 = Math.abs(n);
+                        long n2 = Math.abs(d);
+                        int gcd = 1;
+
+                        for (int k = 1; k <= n1 && k <= n2; k++) {
+                                if (n1 % k == 0 && n2 % k == 0)
+                                        gcd = k;
+                        }
+
+                        return gcd;
+                }
+
+                // Return numerator
+                public long getNumerator() {
+                        return numerator;
+                }
+
+                // Return denominator
+                public long getDenominator() {
+                        return denominator;
+                }
+
+                // Add a rational number to this rational
+                public Rational add(Rational secondRational) {
+                        long n = numerator * secondRational.getDenominator()
+                                        + denominator * secondRational.getNumerator();
+                        long d = denominator * secondRational.getDenominator();
+                        return new Rational(n, d);
+                }
+
+                // Subtract a rational number from this rational
+                public Rational subtract(Rational secondRational) {
+                        long n = numerator * secondRational.getDenominator()
+                                        - denominator * secondRational.getNumerator();
+                        long d = denominator * secondRational.getDenominator();
+                        return new Rational(n, d);
+                }
+
+                // Multiply a rational number to this rational
+                public Rational multiply(Rational secondRational) {
+                        long n = numerator * secondRational.getNumerator();
+                        long d = denominator * secondRational.getDenominator();
+                        return new Rational(n, d);
+                }
+
+                // Divide a rational number from this rational
+                public Rational divide(Rational secondRational) {
+                        long n = numerator * secondRational.getDenominator();
+                        long d = denominator * secondRational.numerator;
+                        return new Rational(n, d);
+                }
+
+                public String toString() {
+                        if (denominator == 1)
+                                return numerator + "";
+                        else
+                                return numerator + "/" + denominator;
+                }
+
+                // Override the equals method in the Object class
+                public boolean equals(Object other) {
+                        if ((this.subtract((Rational) (other))).getNumerator() == 0)
+                                return true;
+                        else
+                                return false;
+                }
+
+                // Implement the abstract intValue method in Number
+                public int intValue() {
+                        return (int) doubleValue();
+                }
+
+                // Implement the abstract floatValue method in Number
+                public float floatValue() {
+                        return (float) doubleValue();
+                }
+
+                // Implement the doubleValue method in Number
+                public double doubleValue() {
+                        return numerator * 1.0 / denominator;
+                }
+
+                // Implement the abstract longValue method in Number
+                public long longValue() {
+                        return (long) doubleValue();
+                }
+
+                // Implement the compareTo method in Comparable
+                public int compareTo(Rational o) {
+                        if (this.subtract(o).getNumerator() > 0)
+                                return 1;
+                        else if (this.subtract(o).getNumerator() < 0)
+                                return -1;
+                        else
+                                return 0;
+                }
+
+                public static Rational parseRationalNumber(String s) {
+                        if (s.contains("/")) {
+                                String words[] = s.split("/");
+                                long numerator = Long.parseLong(words[0].trim());
+                                long denominator = Long.parseLong(words[1].trim());
+                                return new Rational(numerator, denominator);
+                        } else {
+                                return new Rational(Long.parseLong(s.trim()), 1);
+                        }
                 }
         }
-    }
 }
